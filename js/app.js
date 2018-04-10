38b8f48962ecddcede58aa23b8e1dadf1f6d90a3
kingsley <kingzpacking@gmail.com>
2018-04-10 15:39:42 +0100

    Pushing to github

--- js/app.js
+++ js/app.js
@@ -14,20 +14,22 @@ new Vue({
         },
         attack: function(){
             this.monsterHealth -= this.calculateDamage(3, 10);
-            if(this.checkWin()){
-                return;
-            }
-            this.playerHealth -= this.calculateDamage(5, 12);
-            this.checkWin();
+            this.monstaerAttacks();            
+            
         },
         specialAttack: function(){
-
+            this.monsterHealth -= this.calculateDamage(10, 20);
+            this.monstaerAttacks(); 
         },
         heal: function(){
 
         },
         giveUp: function(){
 
+        },
+        monstaerAttacks: function(){
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
         },
         calculateDamage: function(min, max){
             return Math.max(Math.floor(Math.random() * max) + 1, min);
