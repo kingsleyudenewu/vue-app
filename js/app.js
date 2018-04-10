f922efb577b7212a3321caa274399e81b4a094cb
kingsley <kingzpacking@gmail.com>
2018-04-10 08:22:20 +0100

    First commit for my vue js application

---
+++ js/app.js
@@ -0,0 +1,57 @@
+
+new Vue({
+    el: '#app',
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false
+    },
+    methods: {
+        startGame: function(){
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        attack: function(){
+            this.monsterHealth -= this.calculateDamage(3, 10);
+            if(this.checkWin()){
+                return;
+            }
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
+        },
+        specialAttack: function(){
+
+        },
+        heal: function(){
+
+        },
+        giveUp: function(){
+
+        },
+        calculateDamage: function(min, max){
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin: function(){
+            if(this.monsterHealth <= 0){
+                if(confirm('You won! New Game')){
+                    this.startGame();
+                }
+                else{
+                    this.gameIsRunning = false;
+                }                 
+                return true;
+            }
+            else if(this.playerHealth <= 0){
+                if(confirm('You lost! New Game')){
+                    this.startGame();
+                }
+                else{
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}); 
